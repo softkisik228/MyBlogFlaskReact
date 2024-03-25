2dadd7760b16d8fbbd976dfe097466536ba22337
softkisik228 <122627181+softkisik228@users.noreply.github.com>
2024-03-26 00:06:05 +0300

    feat: added function to make a post in db

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import psycopg2
+from config import host, password, user, db_name
 
 app = Flask(__name__)
 
@@ -10,8 +12,31 @@ def index():
 
 @app.route('/', methods=['GET', 'POST'])
 def add_post():
-    data = request.data
+    data = request.args.to_dict()
     print(data)
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"""INSERT INTO posts(post, nickname, date) VALUES
+                ('{data['post']}', '{data['nickname']}', '{data['date']}');""")
+
+            connection.commit()
+            print('ok')
+    except Exception as _ex:
+        print('[INFO] Error while working with PostgreSQL', _ex)
+    finally:
+        if connection:
+            connection.close()
+            print('[INFO] PostgreSQL connection closed')
+
     
 if __name__ == '__main__':
     app.run(debug=True) 
